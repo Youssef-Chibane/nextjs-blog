8609877ffaeb765f06e7a68fb6b24cea815e3958
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-03-07 23:54:41 +0000

    install&config kinde

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+  return (
+    <div className="py-6">
+      <h1 className="text-3xl font-bold tracking-tight mb-8">
+        Dashboard
+      </h1>
+    </div>
+  );
+}
