26ecef63b6329f021b9ea37a484f8a125f289f30
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-03-07 23:31:44 +0000

    fetching db data using prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,37 @@
-import Image from "next/image";
+import { prisma } from "@/lib/prisma";
 
-export default function Home() {
+async function getData() {
+  const data = await prisma.blogPost.findMany({
+    select: {
+      title: true,
+      content: true,
+      imageUrl: true,
+      authorName: true,
+      authorImage: true,
+      id: true,
+      createdAt: true,
+    },
+  });
+
+  return data;
+}
+
+export default async function Home() {
+  const data = await getData();
   return (
-    <div>Hello from home page</div>
+    <div className="py-6">
+      <h1 className="text-3xl font-bold tracking-tight mb-8">Latest Posts</h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {data.map((item) => (
+          <div key={item.id}>
+            <h2>
+              <strong>{item.title}: </strong>
+              {item.content}
+            </h2>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
