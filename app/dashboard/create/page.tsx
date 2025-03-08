4267834d0ac47d7b92cf61d9ee981e91bde8ecc3
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-03-08 14:55:58 +0000

    create post card/form

---
+++ app/dashboard/create/page.tsx
@@ -0,0 +1,44 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Label } from "@radix-ui/react-label";
+
+export default function CreatePage() {
+  return (
+    <div>
+      <Card className="max-w-lg mx-auto">
+        <CardHeader>
+          <CardTitle>Create Post</CardTitle>
+          <CardDescription>
+            Create a new post to share with the world
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form className="flex flex-col gap-4">
+            <div className="flex flex-col gap-2">
+              <Label>Title</Label>
+              <Input required type="text" placeholder="Title" />
+            </div>
+            <div className="flex flex-col gap-2">
+              <Label>Content</Label>
+              <Textarea required placeholder="Content" />
+              <div className="flex flex-col gap-2">
+                <Label>Image Url</Label>
+                <Input required type="url" placeholder="Image Url" />
+              </div>
+            </div>
+
+            <Button>Create Post</Button>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
