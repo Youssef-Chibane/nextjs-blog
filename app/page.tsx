baf431d2a7a35445ffe4e7c83c5e4efa1e3e1fb1
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-03-09 22:46:26 +0000

    cleaning

--- app/page.tsx
+++ app/page.tsx
@@ -22,16 +22,7 @@ export default async function Home() {
     <div className="py-6">
       <h1 className="text-3xl font-bold tracking-tight mb-8">Latest Posts</h1>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {data.map((item) => (
-          <div key={item.id}>
-            <h2>
-              <strong>{item.title}: </strong>
-              {item.content}
-            </h2>
-          </div>
-        ))}
-      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4"></div>
     </div>
   );
 }
