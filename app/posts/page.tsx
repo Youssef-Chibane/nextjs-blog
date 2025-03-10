c45164159a18ab4c7b648cba2b49c7c696a6cba9
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-03-10 01:07:32 +0000

    all posts count

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -14,10 +14,11 @@ async function getData() {
 
 export default async function PostsPage() {
   const data = await getData();
+  const count = await prisma.blogPost.count();
   return (
-    <div className="py-6">
+    <div>
       <h1 className="py-6 text-3xl font-bold tracking-tight" id="latest-posts">
-        Latest Blog Articles:
+        All Blog Articles({count}):
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {data.map((item) => (
