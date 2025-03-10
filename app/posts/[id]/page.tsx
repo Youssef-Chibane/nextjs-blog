3ee7421e7c7fe86fee5ccdf032886f99037add3b
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-03-10 22:43:55 +0000

    PostPage done

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,4 +1,8 @@
+import { buttonVariants } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
 import { prisma } from "@/lib/prisma";
+import Image from "next/image";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 
 interface PageProps {
@@ -15,5 +19,49 @@ export default async function PostPage({ params }: PageProps) {
   if (!data) {
     return notFound();
   }
-  return <div>page</div>;
+  return (
+    <div className="max-w-3xl mx-auto py-8 px-4">
+      <Link
+        href={"/posts"}
+        className={buttonVariants({ variant: "secondary" })}
+      >
+        Back to posts
+      </Link>
+      <div className="mb-8 mt-6">
+        <h1 className="text-3xl font-bold tracking-tight mb-4">{data.title}</h1>
+        <div className="flex items-center space-x-4">
+          <div className="flex items-center space-x-2">
+            <div className="relative size-10 overflow-hidden rounded-full">
+              <Image
+                src={data.authorImage}
+                alt={data.authorName}
+                fill
+                className="object-cover"
+              />
+            </div>
+            <p className="font-medium">{data.authorName}</p>
+          </div>
+          <p className="text-sm text-gray-500">
+            {new Intl.DateTimeFormat("en-US", {
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            }).format(data.createdAt)}
+          </p>
+        </div>
+      </div>
+      <div className="relative h-[400px] w-full mb-8 overflow-hidden rounded-lg">
+        <Image
+          src={data.imageUrl}
+          alt={data.title}
+          fill
+          className="object-cover"
+          priority
+        />
+      </div>
+      <Card>
+        <CardContent className="text-gray-700">{data.content}</CardContent>
+      </Card>
+    </div>
+  );
 }
