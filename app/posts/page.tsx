c5b1847eef5a5b750fce7e9f11c65c6ca3a7f95a
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-03-10 00:56:02 +0000

    fetch all posts

---
+++ app/posts/page.tsx
@@ -0,0 +1,31 @@
+import BlogPostCard from "@/components/general/BlogPostCard";
+import Hero from "@/components/general/Hero";
+import { prisma } from "@/lib/prisma";
+
+async function getData() {
+  const data = await prisma.blogPost.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return data;
+}
+
+export default async function PostsPage() {
+  const data = await getData();
+  return (
+    <div className="py-6">
+      <h1 className="py-6 text-3xl font-bold tracking-tight" id="latest-posts">
+        Latest Blog Articles:
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {data.map((item) => (
+          <div key={item.id}>
+            <BlogPostCard key={item.id} data={item} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
