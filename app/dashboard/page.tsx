3eabe814115496ac2b923e016000b5735d313021
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-03-11 17:36:54 +0000

    fixed create post link

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -50,12 +50,6 @@ async function BlogPosts() {
           <h1 className="text-3xl font-bold text-gray-800 text-center mb-4">
             No blog posts yet! Start your journey by creating the first one.
           </h1>
-          <Link
-            className={`${buttonVariants()} mt-4`}
-            href={"/dashboard/create"}
-          >
-            Create Post
-          </Link>
         </div>
       ) : (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
