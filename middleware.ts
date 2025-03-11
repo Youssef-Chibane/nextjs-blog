efb712a266deae7a017d0b1dc0019a2faa6cbd25
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-03-11 22:51:48 +0000

    delete /posts from publicPaths

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
 
 export default withAuth(async function middleware() {}, {
-  publicPaths: ["/", "/posts"],
+  publicPaths: ["/"],
 });
 
 export const config = {
